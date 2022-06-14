d40bcd01f477b2f75d15c11f0750530a3e235ac8
重光青空 <shigemitsuaozora@Sora-2.local>
2022-06-14 20:45:13 +0900

    add: create todo seed.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating todos...'
+
+5.times do |i|
+  Todo.create(title: "todo#{i + 1}")
+end
+
+puts '...Finished!'
